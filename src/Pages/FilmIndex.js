666825559b6b0488b64b967a6d57daadd18558b1
kellumdavis <kellumdavis@gmail.com>
2022-05-16 14:03:48 -0400

    Film Index rendering all films

--- src/Pages/FilmIndex.js
+++ src/Pages/FilmIndex.js
@@ -1,5 +1,36 @@
+
+import{ useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 function FilmIndex(props) {
-    return <h1>Films</h1>
+    const [film, setFilm] = useState([]);
+    const url = "https://ghibliapi.herokuapp.com/films";
+   
+    useEffect(() => {
+        const getFilm = async () => {
+            try {
+              const response = await fetch(url);
+              const data = await response.json();
+              setFilm(data);
+            } catch (err) {
+              console.log("Ran into a problem ERROR");
+            }
+          };
+    getFilm()}, []);
+    return (
+      <div>
+        {film.map((films, idx) => {
+          const id = films.url.split("/films/").slice(1);
+          return (
+            <div key={idx}>
+              <Link to={`/filmshow/${id}`} >
+                <h2>{films.title}</h2>
+                <img src={films.image} alt={films.title}/>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
 }
 
 export default FilmIndex;
