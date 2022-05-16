9b487866187645b7199ae4604d0ce1c4dc65f5d5
kellumdavis <kellumdavis@gmail.com>
2022-05-16 12:01:43 -0400

    Working links in Header/Nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,22 @@
+import {Link} from 'react-router-dom'
+
 function Header(props) {
-    return <h1>Header</h1>;
+    return (
+    <header>
+        <h1>Studio Ghibli</h1>
+        <nav>
+            <Link to='/'>
+                <div>Home</div>
+            </Link>
+            <Link to='/films'>
+                <div>Films</div>
+            </Link>
+            <Link to='/about'>
+                <div>About</div>
+            </Link>
+        </nav>
+    </header>
+        )
   }
   
   export default Header;
