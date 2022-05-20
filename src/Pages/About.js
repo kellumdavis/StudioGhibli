dc09dab3c236ea57908130f5f34bcfbc440e73fb
kellumdavis <kellumdavis@gmail.com>
2022-05-19 22:11:10 -0400

    styled about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,44 @@
 function About(props) {
-    return <h1>About</h1>;
-  }
-  
-  export default About;
+  return (
+      <div className="aboutContainer">
+        <h1>About Studio Ghibli</h1>
+      <div className="aboutMain">
+        <div className="aboutImage">
+          <img
+            src="https://pbs.twimg.com/media/EMh7d_GUUAAQfzZ?format=png&name=small"
+            alt="Studio Ghibli Founders Hayao Miyazaki and Isao Takahata"
+          ></img>
+          <p>
+            <em>Studio Ghibli Founders Hayao Miyazaki and Isao Takahata</em>
+          </p>
+        </div>
+        <div className="aboutText">
+        <p>
+          Studio Ghibli was founded in 1985 by animated film directors Isao
+          Takahata and Hayao Miyazaki, and has produced twenty-two
+          feature-length films. Most Studio Ghibli films ranked number one at
+          the box office in Japan in the year in which they were released.
+          SPIRITED AWAY, directed by Hayao Miyazaki and released in 2001, is the
+          all-time highest grossing film in Japan, earning over 30 billion yen
+          at the box office.</p>
+          <p>Studio Ghibli films have garnered numerous awards and critical acclaim
+          from film critics and animation specialists around the world. SPIRITED
+          AWAY was awarded the Golden Bear as the Best Feature Film at the 2002
+          Berlin International Film Festival and won the 2002 Academy Award for
+          Best Animated Feature Film. In October 2001, Studio Ghibli, in
+          conjunction with The Tokuma Memorial Cultural Foundation for
+          Animation, founded the Ghibli Museum, Mitaka, designed by Hayao
+          Miyazaki.</p>
+          <p>THE WIND RISES (2013), THE TALE OF THE PRINCESS KAGUYA (2013), WHEN
+          MARNIE WAS THERE (2014) and THE RED TURTLE (2016), the last four films
+          released by Studio Ghibli, have earned the studio four consecutive
+          Academy Award nominations for Best Animated Feature Film. The studio
+          is currently working on a new production.
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
